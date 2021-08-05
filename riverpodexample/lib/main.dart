f0ea4e364d2b8a7c834c262baff5076aa8eb635d
Tharindu97 <tharindukavishna@gmail.com>
2021-08-05 16:57:52 +0530

    changeNotifier completed

--- riverpodexample/lib/main.dart
+++ riverpodexample/lib/main.dart
@@ -3,6 +3,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() => runApp(ProviderScope(child: MyApp()));
 
+class IncrementNotifier extends ChangeNotifier {
+  int _value = 0;
+  int get value => _value;
+
+  void increment() {
+    _value++;
+    notifyListeners();
+  }
+}
+
+final incrementProvider = ChangeNotifierProvider((ref) => IncrementNotifier());
 final greetingProvider = Provider((ref) => 'Hellow Riverpod1!');
 
 class MyApp extends StatelessWidget {
@@ -16,14 +27,42 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Consumer(builder: (context, watch, child) {
-            final greeting = watch(greetingProvider);
-            return Text(greeting);
+            final incrementNotifier = watch(incrementProvider);
+            return Text(incrementNotifier.value.toString());
           }),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.read(incrementProvider).increment();
+          },
+          child: Icon(Icons.add),
         ),
       ),
     );
   }
 }
+
+// Example 02
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Riverpod Tutorial',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Riverpod Tutorial"),
+//         ),
+//         body: Center(
+//           child: Consumer(builder: (context, watch, child) {
+//             final greeting = watch(greetingProvider);
+//             return Text(greeting);
+//           }),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 // Example 01
 
